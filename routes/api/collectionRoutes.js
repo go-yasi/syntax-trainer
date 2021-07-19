9b61e3a654bce9479e1ed95d391d6f374330e0d3
Joshua Galvez <joshuagalvez119@gmail.com>
2021-07-19 11:51:39 -0700

    relationshiup fixes

--- routes/api/collectionRoutes.js
+++ routes/api/collectionRoutes.js
@@ -1,8 +1,43 @@
 const router = require('express').Router();
-const { Collection } = require('../../models/');
+const { Collection, Snippet } = require('../../models/');
 
 // GET all homepage collections
 
+router.get('/', async (req, res) => {
+    try {
+      const collectionData = await Collection.findAll({
+        where: {homepage: true},
+        include: [{ model: Snippet}]
+      });
+  
+      if (!collectionData) {
+        res.status(404).json({ message: 'No collection found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(collectionData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 // GET collections by id
+
+router.get('/:id', async (req, res) => {
+    try {
+      const collectionData = await Collection.findByPk(req.params.id, {
+        include: [{ model: Snippet}]
+      });
+  
+      if (!collectionData) {
+        res.status(404).json({ message: 'No collection found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(collectionData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;

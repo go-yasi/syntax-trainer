8c46771b0ed5f3f06ba649703a20b7f901cdedb9
Tai Mujarin <tai.mujarin@gmail.com>
2021-07-19 14:03:11 -0700

    added to snippet route

--- routes/api/snippetRoutes.js
+++ routes/api/snippetRoutes.js
@@ -1,12 +1,52 @@
 const router = require('express').Router();
-const { Snippet } = require('../../models');
+const { Snippet, User } = require('../../models');
 
 // GET snippet by collection_id
+router.get('/', async (req, res) => {
+    try {
+      const snippetData = await Snippet.findAll();
+      res.status(200).json(snippetData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
 // GET snippet by user
+router.get('/user/:id', async (req, res) => {
+    try {
+      const snippetData = await Snippet.findByPk({
+          includes: [{ model: User }]
+      });
+      res.status(200).json(snippetData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
 // POST snippet by user
+router.post('/', async (req, res) => {
+    try {
+      const snippetData = await Snippet.create(req.body);
+      res.status(200).json(snippetData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
 // DELETE snippet created by user
+router.delete('/:id', async (req, res) => {
+    try {
+      const snippetData = await Snippet.destroy({
+        where: { id: req.params.id }
+      });
+      if (!snippetData) {
+        res.status(404).json({ message: 'No snippet with this id!' });
+        return;
+      }
+      res.status(200).json(snippetData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;

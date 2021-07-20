79b3691c68692469d6c30bf9ac427de1d9459dd5
Tai Mujarin <tai.mujarin@gmail.com>
2021-07-20 14:17:18 -0700

    minor changes to card components

--- client/src/components/HighScoreCard/index.js
+++ client/src/components/HighScoreCard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+// import "./style.css";
 // import  from "";
 
 function HighScoreCard(props) {
@@ -9,11 +9,11 @@ function HighScoreCard(props) {
             <h1>High Scores</h1>
         </div>
         <div className="scores">
-            <h3 className="">{props.user.username}-{props.snippet.title}-{Props.scores.value}</h3>
-            <h3 className="">{props.user.username}-{props.snippet.title}-{Props.scores.value}</h3>
-            <h3 className="">{props.user.username}-{props.snippet.title}-{Props.scores.value}</h3>
-            <h3 className="">{props.user.username}-{props.snippet.title}-{Props.scores.value}</h3>
-            <h3 className="">{props.user.username}-{props.snippet.title}-{Props.scores.value}</h3>
+            <h3 className="">{props.user.username}-{props.snippet.title}-{props.scores.value}</h3>
+            <h3 className="">{props.user.username}-{props.snippet.title}-{props.scores.value}</h3>
+            <h3 className="">{props.user.username}-{props.snippet.title}-{props.scores.value}</h3>
+            <h3 className="">{props.user.username}-{props.snippet.title}-{props.scores.value}</h3>
+            <h3 className="">{props.user.username}-{props.snippet.title}-{props.scores.value}</h3>
         </div>
     </section>
   );

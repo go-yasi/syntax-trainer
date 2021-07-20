269e52033e0e83373c64b972f73df688ec0bbe04
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-20 10:38:48 -0700

    Added Tom's beautiful seed files

---
+++ seeds/seed.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+const { Collection, Score, Snippet, User } = require('../models');
+
+const collectionSeed = require('./collection.json');
+const userSeed = require('./user.json');
+// const snippetSeed = require('./snippet.json');
+const scoreSeed = require('./score.json');
+
+const seedDatabase = () => {
+    return sequelize.sync({ force: true }).then(() => {
+      Collection.bulkCreate(collectionSeed).then(() => {
+        User.bulkCreate(userSeed).then(() => {
+            // Snippet.bulkCreate(snippetSeed).then(() => {
+                Score.bulkCreate(scoreSeed).then(() => {
+          console.log('All Seeds Planted');
+        });
+      });
+    });
+});
+    };
+
+
+
+seedDatabase();

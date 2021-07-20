fbc688075b04778098b4c94dd0f181e1a2aa471b
Joshua Galvez <joshuagalvez119@gmail.com>
2021-07-20 12:02:43 -0700

    login form state varibales and onsubmit

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,21 +1,32 @@
-import React from "react";
+//import React from "react";
+import React, { useState } from "react";
 
 function LoginForm() {
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("username is " + username);
+        console.log("password is " + password);
+    };
+
     return (
         <div>
             <h2>Login Form</h2>
             <section class="login-block">
                 {/* <!-- Login Form --> */}
-                <form class="form login-form">
+                <form class="form login-form" onSubmit={handleSubmit}>
                     <div class="form-group">
                         {/* <!-- Username --> */}
                         <label for="username-login">username</label>
-                        <input class="form-input" type="text" id="username-login" placeholder="username"></input>
+                        <input class="form-input" type="text" id="username-login" placeholder="username" onChange={e => setUsername(e.target.value)}></input>
                     </div>
                     <div class="form-group">
                         {/* <!-- Password --> */}
                         <label for="password-login">password</label>
-                        <input class="form-input" type="password" id="password-login" placeholder="password"></input>
+                        <input class="form-input" type="password" id="password-login" placeholder="password" onChange={e => setPassword(e.target.value)}></input>
                     </div>
                     <div class="form-group">
                         {/* <!-- Login Button --> */}

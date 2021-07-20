8ceaf35bc0fbbac418750a071455c5173f108577
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-20 13:11:44 -0700

    Craeted new folders for needed components

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -15,25 +15,25 @@ function LoginForm() {
     return (
         <div>
             <h2>Login Form</h2>
-            <section class="login-block">
+            <section className="login-block">
                 {/* <!-- Login Form --> */}
-                <form class="form login-form" onSubmit={handleSubmit}>
-                    <div class="form-group">
+                <form className="form login-form" onSubmit={handleSubmit}>
+                    <div className="form-group">
                         {/* <!-- Username --> */}
                         <label for="username-login">username</label>
-                        <input class="form-input" type="text" id="username-login" placeholder="username" onChange={e => setUsername(e.target.value)}></input>
+                        <input className="form-input" type="text" id="username-login" placeholder="username" onChange={e => setUsername(e.target.value)}></input>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         {/* <!-- Password --> */}
                         <label for="password-login">password</label>
-                        <input class="form-input" type="password" id="password-login" placeholder="password" onChange={e => setPassword(e.target.value)}></input>
+                        <input className="form-input" type="password" id="password-login" placeholder="password" onChange={e => setPassword(e.target.value)}></input>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         {/* <!-- Login Button --> */}
-                        <button class="green-btn login-button" type="submit">Log In</button>
+                        <button className="green-btn login-button" type="submit">Log In</button>
                     </div>
-                    <div class="form-group">
-                        <a class="l-s-cta" href="/signup">Need to sign up instead?</a>
+                    <div className="form-group">
+                        <a className="l-s-cta" href="/signup">Need to sign up instead?</a>
                     </div>
                 </form>
             </section>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 class Home extends Component {
     render() {
         return (
-            <h1>Hello World! You're Home</h1>
+            <div>
+                <h1>Hello World! You're Home</h1>
+            </div>
+            
         );
     }
 }

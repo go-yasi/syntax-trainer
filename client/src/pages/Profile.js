695e58269f80bdb33b0765f09514e307d1ac9cdf
Joshua Galvez <joshuagalvez119@gmail.com>
2021-07-20 14:40:55 -0700

    profile page setup

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import ProfileScoreCard from "../components/ProfileScoreCard";
 import UserCard from "../components/UserCard";
+import SnippetCard from "../components/SnippetCard";
 
 class Profile extends Component {
     render() {
+        const snip = {
+            title: "snip snip"
+        }
+        const snipScore = {
+            value: 3
+        }
         return (
             <div>
                 <h1>Cool Profile</h1>
                 <UserCard />
-                <ProfileScoreCard />
+                <ProfileScoreCard snippet={snip} scores={snipScore}/>
+                <SnippetCard />
+                <SnippetCard />
             </div>
         );
     }

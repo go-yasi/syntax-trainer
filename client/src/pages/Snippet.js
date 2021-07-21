265cfedb14f6b26e7823f832be2d0e0f9278cc66
Joshua Galvez <joshuagalvez119@gmail.com>
2021-07-21 11:18:01 -0700

    proxy server stuff

--- client/src/pages/Snippet.js
+++ client/src/pages/Snippet.js
@@ -1,15 +1,32 @@
-import React, { Component } from "react";
+import React, { useEffect, useState, Component } from "react";
+import API from "../utils/API"
 
-class Snippet extends Component {
-    
-    render() {
-        return (
-            <div>
-                <h1>The Snippet!</h1>
-                <p></p>
-            </div>
-        );
-    }
+
+
+function Snippet() {
+    const [snippet, setSnippet] = useState({});
+
+    useEffect(() => {
+        loadSnippets();
+        /*        fetch("/api/snippet/11")
+        .then(res => console.log(res))*/
+      }, []);
+
+    function loadSnippets() {
+        API.fetchSnippet()
+          .then(res =>{
+              setSnippet(res.data);
+              console.log(res.data);
+          })
+          .catch(err => console.log(err));
+      }
+    return (
+        <div>
+            <h1>The Snippet!</h1>
+            <h3>{snippet.title}</h3>
+            <code>{snippet.code}</code>
+        </div>
+    );
 }
 
 export default Snippet;

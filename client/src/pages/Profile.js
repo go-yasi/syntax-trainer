f1c766e3c8399f07f09d8119970f8d6a84a5e8bf
Tai Mujarin <tai.mujarin@gmail.com>
2021-07-21 15:59:48 -0700

    made changes to profilejs

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,26 +1,72 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import ProfileScoreCard from "../components/ProfileScoreCard";
 import UserCard from "../components/UserCard";
 import SnippetCard from "../components/SnippetCard";
+import API from "../utils/API"
 
-class Profile extends Component {
-    render() {
-        const snip = {
-            title: "snip snip"
-        }
-        const snipScore = {
-            value: 3
-        }
-        return (
-            <div>
-                <h1>Cool Profile</h1>
-                <UserCard />
-                <ProfileScoreCard snippet={snip} scores={snipScore}/>
-                <SnippetCard />
-                <SnippetCard />
-            </div>
-        );
+function Profile() {
+    const [profile, setProfile] = useState({
+        score: [],
+        users: {}
+    });
+
+    useEffect(() => {
+        loadAll();
+    }, []);
+
+    useEffect(() => {
+        console.log(profile)
+    }, [profile]);
+
+    function loadProfile(id){
+        API.fetchUser(1)
+            .then(res =>{
+                setProfile({...profile, users:res.data});
+                console.log(res.data);
+            })
+            .catch(err => console.log(err));
     }
+    function loadScores(id){
+        API.fetchUserScores(1)
+            .then(res =>{
+                setProfile({...profile, score:res.data});
+                console.log(res.data);
+            })
+            .catch(err => console.log(err));
+    }
+    async function loadAll(){
+        const load = await loadProfile();
+        const load2 = await loadScores();
+        // console.log(load);
+    }
+    
+    return (
+        <div>
+            <h1>Cool Profile</h1>
+                {profile.users ? (
+                <div>
+                    <UserCard username={profile.users.username} bio={profile.users.bio} avatar={profile.users.avatar}/>
+                </div>
+                ):(
+                    <div>
+                        Loading Bios
+                    </div>
+                )}
+                {profile.score.length ? (
+                    <div>
+                        <ProfileScoreCard snippet={profile.score[0].snippet_id} value={profile.score[0].value}/>
+                        <ProfileScoreCard snippet={profile.score[1].snippet_id} value={profile.score[1].value}/>
+                    </div>
+                ):(
+                    <div>
+                        Loading Score Card
+                    </div>
+                )}
+            {/* <ProfileScoreCard snippet={profile} scores={profile}/> */}
+            {/* <SnippetCard title={profile.snippets[0].title} language={profile.snippets[0].language} code={profile.snippets[0].code}/>
+            <SnippetCard title={profile.snippets[1].title} language={profile.snippets[1].language} code={profile.snippets[1].code}/> */}
+        </div>
+    );
 }
 
 export default Profile;

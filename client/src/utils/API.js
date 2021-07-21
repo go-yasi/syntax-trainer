788df2e32c0765d618b7e8110362777e5af75282
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-21 11:42:41 -0700

    Adding routes to API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,32 @@
 import axios from "axios";
 
 export default {
+    // USER ROUTES
+    fetchUser: function(id) {
+        return axios.get("/api/users/" + id)
+    },
+    createUser: function(newUser) {
+        return axios.post("/api/users/", newUser)
+    },
+    // needs login & logout
+    //   logoutUser: function() {
+    //       return axios.post("/api/users/logout")
+    //   }
 
-    fetchSnippet: function() {
-        return axios
-          .get("/api/snippet/11")
-         
-         
-      }
+    // COLLECTION ROUTES
+    fetchCollections: function() {
+        return axios.get("/api/collection")
+    },
+    fetchCollection: function(id) {
+        return axios.get("/api/collection" + id)
+    },
+
+    // SNIPPET ROUTES
+    fetchSnippets: function() {
+        return axios.get("/api/snippet")
+    },
+    fetchSnippet: function(id) {
+        return axios.get("/api/snippet" + id)
+    },
 
 };
